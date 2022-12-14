75ebf5624bd077fa780b09da4c6620b684ba2c19
YonathanFabiano <yonathan.fabiano@si.ukdw.ac.id>
2022-12-14 18:19:47 +0700

    Pertemuan 14

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
+using System.Globalization;
+using Microsoft.JSInterop;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorAppWasm;
 using MudBlazor.Services;
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,5 +16,30 @@ builder.Services.AddScoped(sp =>
     });
 
 builder.Services.AddMudServices();
+builder.Services.AddLocalization();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-CL");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-CL");
 
 await builder.Build().RunAsync();
+
+// var host = builder.Build();
+
+// CultureInfo culture;
+// var js = host.Services.GetRequiredService<IJSRuntime>();
+// var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+// if (result != null)
+// {
+//     culture = new CultureInfo(result);
+// }
+// else
+// {
+//     culture = new CultureInfo("en-US");
+//     await js.InvokeVoidAsync("blazorCulture.set", "en-US");
+// }
+
+// CultureInfo.DefaultThreadCurrentCulture = culture;
+// CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+// await host.RunAsync();
